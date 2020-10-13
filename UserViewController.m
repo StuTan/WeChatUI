0621531ab72871f038fd720f67158af15da41cbf
StuTan <50095108+StuTan@users.noreply.github.com>
2020-10-13 10:52:14 +0800

    Add files via upload
    Add a file

---
+++ UserViewController.m
@@ -0,0 +1,68 @@
+//
+//  UserViewController.m
+//  WechatUI
+//
+//  Created by tanwenmeng on 2020/10/9.
+//
+
+#import "UserViewController.h"
+#import "UserTableViewCell.h"
+
+@interface UserViewController ()<UITableViewDelegate,UITableViewDataSource>
+
+@property (nonatomic, strong) UITableView *tableView;
+
+@end
+
+@implementation UserViewController
+
+- (instancetype)init{
+    self = [super init];
+    if(self){
+        self.title = @"好友";
+    }
+    return self;
+}
+ 
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    self.view.backgroundColor = [UIColor whiteColor];
+    
+    _tableView = [[UITableView alloc] initWithFrame:self.view.bounds];
+    _tableView.dataSource = self;
+    _tableView.delegate = self;
+    
+    //隐藏多余分割线
+    self.tableView.tableFooterView = [[UIView alloc] initWithFrame:CGRectZero];
+    [self.view addSubview:_tableView];
+    
+     
+    // Do any additional setup after loading the view.
+}
+
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section{
+    return 1;
+}
+
+- (CGFloat)tableView:(UITableView *)tableView heightForRowAtIndexPath:(NSIndexPath *)indexPath {
+    return 100;
+}
+
+- (void) tableView:(UITableView *)tableView didSelectRowAtIndexPath:(NSIndexPath *)indexPath{
+    UIViewController *controller = [[UIViewController alloc] init];
+    controller.title = [NSString stringWithFormat:@"%@",@(indexPath.row)];
+    [self.navigationController pushViewController:controller animated:YES];
+}
+
+- (UITableViewCell *) tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath{
+    
+    UserTableViewCell *cell = [[UserTableViewCell alloc] initWithStyle:UITableViewCellStyleSubtitle reuseIdentifier:@"cell"];
+    
+    if (cell == nil) {
+        cell = [[UserTableViewCell alloc] initWithStyle:UITableViewCellStyleValue1 reuseIdentifier:@"cell"];
+    }
+     
+    
+    return cell;
+};
+@end
